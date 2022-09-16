4749d8a3cde1a2a4154c3bca59decc16eb5e1642
taeyoung <rlaxodud911@gmail.com>
2022-09-16 23:22:55 +0900

    프로젝트 준비

---
+++ backend/config/dev.py
@@ -0,0 +1,11 @@
+from config.common import *
+import os
+
+DEBUG = True
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'flaskinstagram.db'))
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+PROPGATE_EXECPTIONS = True
+JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
+SECRET_KEY = os.environ["APP_SECRET_KEY"]
+JWT_BLACKLIST_ENABLED = True
+JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]

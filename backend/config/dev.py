c29632449ada8116bb8897cf5e8a0500315d29c6
taeyoung <rlaxodud911@gmail.com>
2022-10-07 20:12:11 +0900

    jwt_secret_key 수정

--- backend/config/dev.py
+++ backend/config/dev.py
@@ -6,8 +6,7 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(
     os.path.join(BASE_DIR, 'flaskinstagram.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 PROPGATE_EXECPTIONS = True
-# JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
-JWT_SECRET_KEY = 'flaskinstagramsecretkey'
+JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
 SECRET_KEY = os.environ["APP_SECRET_KEY"]
 JWT_BLACKLIST_ENABLED = True
 JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]

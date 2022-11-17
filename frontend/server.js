fe19747df3e56d5d76c5d42b93da8864c53fb3bc
taeyoung <rlaxodud911@gmail.com>
2022-11-17 22:56:01 +0900

    Ep15: frontend 폴더 구조 변경

--- frontend/server.js
+++ frontend/server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var app = express();
 
 // static 파일들(js, css, img)의 기본 디렉토리로서 상위 경로(../) 를 사용하겠다!
-app.use(express.static(path.join(__dirname, '..')));
+app.use(express.static(path.join(__dirname)));
 
 // 3000번 대에서 서버를 열고,
 app.listen(3000, (err) => {
@@ -13,9 +13,9 @@ app.listen(3000, (err) => {
 });
 
 app.get('/flastagram/posts', function (req, res) {
-  res.sendFile(path.join(__dirname, '..', 'post_list.html'));
+  res.sendFile(path.join(__dirname, 'assets', 'html', 'post_list.html'));
 });
 
 app.get('/flastagram/post-create', function (req, res) {
-  res.sendFile(path.join(__dirname, '..', 'post_create.html'));
+  res.sendFile(path.join(__dirname, 'assets', 'html', 'post_create.html'));
 });

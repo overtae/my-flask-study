e0b8e0592d2a3ac60b6aac7d8bef8ec4da4b4837
taeyoung <rlaxodud911@gmail.com>
2023-01-24 15:12:36 +0900

    Ep21[BE]: 한글 인코딩 추가 및 오타 수정

--- backend/config/common.py
+++ backend/config/common.py
@@ -10,8 +10,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 UPLOADED_IMAGES_DEST = os.path.join(BASE_DIR, "static", "images")
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-PROPGATE_EXECPTIONS = True
+PROPAGATE_EXCEPTIONS = True
 JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
 SECRET_KEY = os.environ["APP_SECRET_KEY"]
 JWT_BLACKLIST_ENABLED = True
 JWT_BLACKLIST_TOKEN_CHECKS = ["access", "refresh"]
+JSON_AS_ASCII = False
